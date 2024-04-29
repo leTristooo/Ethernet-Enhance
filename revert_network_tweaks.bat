539b1f17af06f567d87ad5c3f84a3112ea143bd2
leTristooo <116842584+leTristooo@users.noreply.github.com>
2024-04-29 17:02:56 +0200

    Update revert_network_tweaks.bat

--- revert_network_tweaks.bat
+++ revert_network_tweaks.bat
@@ -11,7 +11,7 @@ echo Nagle's Algorithm re-enabled.
 echo.
 
 :: Reset Network Throttling Index to default value
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v NetworkThrottlingIndex /t REG_DWORD /d 10 /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v NetworkThrottlingIndex /f
 echo Network Throttling Index reset to default.
 echo.
 
@@ -20,8 +20,32 @@ netsh int tcp set global autotuninglevel=normal
 netsh int tcp set global congestionprovider=default
 netsh int tcp set global ecncapability=default
 netsh int tcp set global timestamps=default
+netsh int tcp set global initialRto=default
+netsh int tcp set global maxSynRetransmissions=default
+netsh int tcp set global nonsackrttresiliency=default
+netsh int tcp set global rsc=default
 netsh int tcp set global chimney=disabled
 echo TCP/IP Settings reset to default.
+echo.
+
+:: Enable Large Send Offload
+netsh int tcp set global lso=enabled
+echo Large Send Offload enabled.
+echo.
+
+:: Enable Receive Side Scaling
+netsh int tcp set global rss=enabled
+echo Receive Side Scaling enabled.
+echo.
+
+:: Enable Packet Coalescing
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DisablePacketCoalescing /f
+echo Packet Coalescing enabled.
+echo.
+
+:: Flush DNS Cache
+ipconfig /flushdns
+echo DNS Cache flushed.
 echo.
 
 echo Network and Ethernet tweaks reverted.
